b6b47aa6563911e820d280a896e485dbe78840b5
Kimberly Fievre <kimfievre@users.noreply.github.com>
2016-09-02 13:30:57 -0500

    Registration final commit

--- API Challenge.js
+++ API Challenge.js
@@ -1,18 +1,7 @@
-//http://challenge.code2040.org/api/register
-//My API Token: d19c018fac18507f87dfe1265295cdbc
-//https://github.com/kimfievre/Code-2040
-
 $(document).ready(function(){
-	// var token = "d19c018fac18507f87dfe1265295cdbc"
-	// var github = "https://github.com/kimfievre/Code-2040"
-	$.post("http://challenge.code2040.org/api/register",
-	{
-		token: "d19c018fac18507f87dfe1265295cdbc"
-		github: https:\/\/github.com/kimfievre/Code2040
-	},
-	function(data){
+	var token = "d19c018fac18507f87dfe1265295cdbc";
+	var github = "https://github.com/kimfievre/Code-2040.git";
+	info = {"token": token, "github": github};
 
-	});
-
-
+	$.post("http://challenge.code2040.org/api/register", info);
 });
